97b78a448c7d473fa86936eeb16fcecd776b69ba
DaveP80 <davidpaq1@gmail.com>
2022-08-08 19:23:54 -0400

    initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,36 +3,57 @@ import java.util.Scanner;
 public class Main {
     static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         FConversionService conversion = new FConversionService(); //dependency injection
         SimpleConversionService simple = new SimpleConversionService(); //dependency injection
         ConversionService conversionService = new ConversionService(conversion);
         ConversionService conversionService1 = new ConversionService(simple);
 
-        while (true) {
+        int choice;
 
-            System.out.println("press 1 to convert to USD, press anyInt to convert to Foreign");
+        do {
+            System.out.println("Currency Conversion menu\n");
+            System.out.print("1.) Convert to USD \n");
+            System.out.print("2.) Convert to Foreign\n");
+            System.out.print("3.) Exit\n");
+            System.out.print("\nEnter Your Menu Choice: ");
+            choice = input.nextInt();
 
-            while (!input.hasNextInt()) {
-                input.nextLine();
+            switch (choice) {
+
+                case 1: {
+                    System.out.print("USD->USD ");
+                    int scan = input.nextInt();
+
+                    Coins c = new Coins(scan);
+
+                    conversionService1.convert(c);
+
+                    break;
+                } //need brackets to contain Coin object
+
+                case 2:
+
+                    System.out.print("USD->foreign ");
+                    int scan = input.nextInt();
+
+                    Coins c = new Coins(scan);
+
+                    conversionService.convert(c);
+                    Thread.sleep(2000);
+                    break;
+
+                case 3:
+                    System.out.println("Exiting Program...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println(choice + " is not a valid Menu Option! Please Select Another.");
+
             }
 
-            if (input.nextInt() == 1) {
-                int scan = input.nextInt();
-
-                Coins c = new Coins(scan);
-
-                conversionService1.convert(c);
-
-            } else {
-                int scan = input.nextInt();
-
-                Coins c = new Coins(scan);
-
-                conversionService.convert(c);
-
-            }
-        }
+        } while (choice != 3);
     }
 }
+
